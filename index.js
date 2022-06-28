3c61d0ca487ee76c7ecbd6453eb22844bff20773
Se3men <semyonsergeev30@gmail.com>
2022-06-28 11:55:07 +0300

    added error processing, check jwt middleware, main logic for users

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const express = require('express');
 const sequelize = require('./db');
 const models = require('./models/models');
 const router = require('./routes/index');
+const path = require('path');
+const errorHandler = require('./middleware/ErrorHandlingMiddlware');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
 app.use('/api', router);
+app.use(errorHandler);
 
 const PORT = process.env.PORT ?? 5000;
 
--- models/models.js
+++ models/models.js
@@ -3,13 +3,13 @@ const { DataTypes } = require('sequelize');
 
 const User = sequelize.define('user', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    firstName: { type: DataTypes.STRING },
-    lastName: { type: DataTypes.STRING },
-    email: { type: DataTypes.STRING, unique: true },
-    password: { type: DataTypes.STRING },
-    sex: { type: DataTypes.ENUM('man', 'woman', 'other') },
-    photo: { type: DataTypes.STRING },
-    registrationDate: {type: DataTypes.STRING},
+    firstName: { type: DataTypes.STRING, allowNull: false },
+    lastName: { type: DataTypes.STRING, defaultValue: null },
+    email: { type: DataTypes.STRING, unique: true, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
+    sex: { type: DataTypes.ENUM('man', 'woman', 'other'), defaultValue: null },
+    photo: { type: DataTypes.STRING, defaultValue: null },
+    registrationDate: {type: DataTypes.STRING, allowNull: false },
 }, { timestamps: false })
 
 module.exports = { User }
